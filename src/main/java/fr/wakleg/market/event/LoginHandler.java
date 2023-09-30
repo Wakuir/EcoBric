64b8b09d0e73919c40c6905a23dd786c9fafafc5
Wakuir <113540865+Wakuir@users.noreply.github.com>
2023-09-30 10:57:42 +0200

    added a message when offline money has been given

--- src/main/java/fr/wakleg/market/event/LoginHandler.java
+++ src/main/java/fr/wakleg/market/event/LoginHandler.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 
 import java.sql.*;
 
@@ -23,17 +24,19 @@ public class LoginHandler implements ServerEntityEvents.Load {
                 String createTableQuery = "CREATE TABLE IF NOT EXISTS " + OFFLINE_MONEY_TABLE + " (player_uuid TEXT, amount INT)";
                 connection.createStatement().execute(createTableQuery);
 
-                String selectQuery = "SELECT player_uuid, amount FROM " + OFFLINE_MONEY_TABLE;
-                ResultSet resultSet = connection.createStatement().executeQuery(selectQuery);
+                String selectQuery = "SELECT amount FROM " + OFFLINE_MONEY_TABLE + " WHERE player_uuid = (?)";
+                PreparedStatement selectStatement = connection.prepareStatement(selectQuery);
+                selectStatement.setString(1, player.getUuidAsString());
+                ResultSet resultSet = selectStatement.executeQuery();
 
-                while (resultSet.next()){
-                    if (player.getUuidAsString().equals(resultSet.getString("player_uuid"))){
-                        MoneyManager.add((IEntityDataSaver) player, resultSet.getInt("amount"));
-                        String deleteQuery = "DELETE FROM " + OFFLINE_MONEY_TABLE + " WHERE (player_uuid) = (?)";
-                        PreparedStatement statement = connection.prepareStatement(deleteQuery);
-                        statement.setString(1, player.getUuidAsString());
-                        statement.executeUpdate();
-                    }
+                if(resultSet.next()){
+                    MoneyManager.add((IEntityDataSaver) player, resultSet.getInt("amount"));
+                    String deleteQuery = "DELETE FROM " + OFFLINE_MONEY_TABLE + " WHERE (player_uuid) = (?)";
+                    PreparedStatement statement = connection.prepareStatement(deleteQuery);
+                    statement.setString(1, player.getUuidAsString());
+                    statement.executeUpdate();
+
+                    player.sendMessage(Text.literal("Vos items on été vendus pour un montant de " + resultSet.getInt("amount") + "$"));
                 }
 
                 connection.close();
